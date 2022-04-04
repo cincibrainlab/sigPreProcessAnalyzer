a96575fc8c081bc1caf8861fb286c009158dc180
Ernie Pedapati <ernest.pedapati@cchmc.org>
2022-04-03 21:25:54 -0400

    added initial GUI analyzer

--- spaController.m
+++ spaController.m
@@ -3,19 +3,35 @@ classdef spaController < handle
     %   Detailed explanation goes here
     
     properties
-        folder = struct();
+        current_folder;  % current user-selected directory
+        current_files;   % all SET files in current directory
     end
     
     methods
         function obj = spaController()
             %SPACONTROLLER Construct an instance of this class
             %   Controller class for Signal Preprocess Analyzer
+            
+            % check if eeglab is running
+            if ~exist('eeg_checkset.m','file') == 2
+                try
+                    eeglab nogui;
+                catch
+                    error("Please add eeglab to MATLAB path.")
+                end
+            end
         end
 
         function [selectedFolder, obj] = openFolder(obj)
             %SELECTFOLDER
-             selectedFolder = uigetdir;
-             obj.folder.currentdata = selectedFolder;
+            selectedFolder = uigetdir;
+            obj.current_folder = selectedFolder;
+            
+            a=dir(fullfile(selectedFolder,'*.set'));                         % Obtains the contents of the selected path.
+            b={a(:).name}';                      % Gets the name of the files/folders of the contents and stores them appropriately in a cell array
+            b(ismember(b,{'.','..'})) = [];      % Removes unnecessary '.' and '..' results from the display.
+            
+            obj.current_files = b;
         end
     end
     
